ac1a996aaca21c0c206cbf3d8f4c59dbb5950f74
PC-20170905CGYU\Administrator <1552044904@qq.com>
2019-03-20 18:26:14 +0800

    modified

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,41 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const ctx = wx.createCanvasContext('firstCanvas')
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    list: [
+      {id:1,src:'../../img/1.png'},
+      {id:2,src: '../../img/2.png'},
+      {id:3,src:'../../img/3.png'}, 
+      {id:4,src:'../../img/4.png'}
+    ],
+    inputValue: '',
+    path:'',
+    focus: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onReady: function (e) {
+   
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  getImg(e){
+    let img = e.target.dataset.src;
+    ctx.drawImage(img, 0, 0,250,250)
+    ctx.draw();
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      path: img
     })
-  }
+  },
+  addText(){
+    ctx.drawImage(this.data.path, 0, 0, 250, 250)
+    ctx.setFontSize(20)
+    ctx.fillText(this.data.inputValue, 50, 20);
+    ctx.draw()
+  },
+  bindKeyInput(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  
 })
